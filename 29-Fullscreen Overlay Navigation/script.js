23b08b99808d165dfd96a5fd23d962637a49cce2
rabiaztoprak <80384765+rabiaztoprak@users.noreply.github.com>
2023-05-11 13:13:51 +0300

    Add files via upload

---
+++ 29-Fullscreen Overlay Navigation/script.js
@@ -0,0 +1,15 @@
+function navigation(){
+    var icon = document.getElementById("icon");
+    var navbar = document.getElementById("navbar");
+
+    if(icon.classList.contains('fa-bars')){
+        icon.classList.remove('fa-bars');
+        icon.classList.add('fa-times');
+        navbar.style.left = '0';
+    }
+    else{
+        icon.classList.remove('fa-times');
+        icon.classList.add('fa-bars');
+        navbar.style.left = '-100%';
+    }
+}
